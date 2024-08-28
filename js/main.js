942ffe25223d56185ee5034ab1f3b8d4c4809b0d
Hiro-Eno <149124193+Hiro-Eno@users.noreply.github.com>
2024-08-28 09:09:38 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 'use strict'
 
-let day = new Date().getDate();
-
-const imagePath = makeImagePath(day);
-
 const calendarImage = document.getElementById("calendar");
-calendarImage.src = imagePath;
+
+let day = today();
 
 function makeImagePath(day){
     const day_ = day < 10 ? `0${day}` : day;
     return `img/${day_}.jpg`;
 }
 
+function today(){
+    let day = new Date().getDate();
+    const todayImagePath = makeImagePath(day);
+    calendarImage.src = todayImagePath;
+    return day;
+}
+
 function previous(){
     day = day === 1 ? 31 : day - 1;
     const preImagePath = makeImagePath(day);
-    const image = document.getElementById("calendar");
-    image.src= preImagePath
+    calendarImage.src= preImagePath
 }
 
 function next(){
     day = day === 31 ? 1 : day + 1;
     const nextImagePath = makeImagePath(day);
-    const image = document.getElementById("calendar");
-    image.src= nextImagePath
+    calendarImage.src= nextImagePath
+}
+
+function random(){
+    for(let i=0; i<9 ; i++){
+        setTimeout(() => {
+            day = Math.round(Math.random()*30) + 1;
+            let randomImagePath = makeImagePath(day);
+            calendarImage.src = randomImagePath;
+        }, 3 * i * i * i);
+    } 
 }
